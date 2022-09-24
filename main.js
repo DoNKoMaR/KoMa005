cfcb637c7daa2bec88da17c430e103a95ad79b3f
KoMaR005 <113529621+DoNKoMaR@users.noreply.github.com>
2022-09-24 08:29:05 +0600

    Venom

--- main.js
+++ main.js
@@ -73,7 +73,7 @@ const { state, saveState } = useSingleFileAuthState(global.authFile)
 const connectionOptions = {
 printQRInTerminal: true,
 auth: state,
-browser: ['TheLoliBot-MD','Edge','1.0.0'],
+browser: ['☣ⒹⓞⓃ ⓀⓞⓂⓐⓇ☣','Edge','1.0.0'],
 }
 
 global.conn = makeWASocket(connectionOptions)
